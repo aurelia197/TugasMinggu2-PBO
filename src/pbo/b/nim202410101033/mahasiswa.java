4a4bb44a37b0f499d3c0e6734d2d6abd4e82dbc0
Aurelia1033 <85263622+aurelia197@users.noreply.github.com>
2021-09-12 13:41:49 +0000

    Update mahasiswa.java

--- src/pbo/b/nim202410101033/mahasiswa.java
+++ src/pbo/b/nim202410101033/mahasiswa.java
@@ -2,3 +2,13 @@ package pbo.b.nim202410101033;
 
 public class Mahasiswa {
 String nama, nim, prodi;
+
+Mahasiswa(String nama, String nim, String prodi){
+ininama = nama;
+ininim = nim;
+iniprodi = prodi;
+}
+
+//Method
+String perkenalan(){
+return String.format
